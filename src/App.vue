a442c730c7c9231f4d11d607d363c5157cad6795
Mohamed Ibrahim <mo7mad369@gmail.com>
2021-01-09 18:43:59 +0200

    تظبيط الدنيا علشان نبدأ نشتغل على روقان

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!-- <router-view /> -->
+    <div v-if="isLogin">
+      <Dashboard user="userName" />
+      <!--  this component will show after login -->
     </div>
-    <router-view />
+    <div v-else>
+      <Login />
+      <!--  this component will show before login -->
+    </div>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Dashboard from "@/components/Dashboard/Dashboard.vue";
+import Login from "@/components/Login/Login.vue";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  data() {
+    return {
+      isLogin: true
+    };
+  },
+  components: {
+    Dashboard,
+    Login
   }
+};
+</script>
+
+<style lang="scss">
+* {
+  // simple reset for all tags
+  margin: 0;
+  padding: 0;
+  color: inherit;
+  box-sizing: border-box;
+  list-style: none;
+  outline: none;
 }
 </style>
---
+++ src/components/Login/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container-login">
+    <LoginForm />
+  </div>
+</template>
+<script>
+import LoginForm from "./form"; // incloud Login Form component
+
+export default {
+  name: "Login",
+  components: { LoginForm }
+};
+</script>
+<style lang="scss" scoped>
+.container-login {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #a62440;
+}
+</style>
